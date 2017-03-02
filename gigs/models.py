6d980a68eaf2a992957f5e7588064af2a7e33f64
andela-jngatia <joan.ngatia@andela.com>
2017-03-02 21:19:26 +0300

    Add venue Model

--- gigs/models.py
+++ gigs/models.py
@@ -1,5 +1,9 @@
 from __future__ import unicode_literals
 
-from django.db import models
+from django.contrib.gis.db import models
+
 
 # Create your models here.
+class Venue(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.PointField()
--- gigs/tests.py
+++ gigs/tests.py
@@ -1,3 +1,31 @@
+import random, factory.django
+from django.contrib.gis.geos import Point
 from django.test import TestCase
-
+from factory.fuzzy import BaseFuzzyAttribute
+from gigs.models import Venue
 # Create your tests here.
+
+
+class FuzzyPoint(BaseFuzzyAttribute):
+    def fuzz(self):
+        return Point(random.uniform(-180.0, 180.0),
+                     random.uniform(-90.0, 90.0))
+
+
+class Venuefactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Venue
+        django_get_or_create = ('name', 'location')
+
+    name = 'Wembley'
+    location = FuzzyPoint()
+
+
+class VenueTest(TestCase):
+    def test_create_venue(self):
+        venue = Venuefactory()
+
+        all_venues = Venue.objects.all()
+        self.assertEqual(len(all_venues), 1)
+        self.assertEqual(venue, all_venues[0])
+        self.assertEqual(all_venues[0].name, 'Wembley')
